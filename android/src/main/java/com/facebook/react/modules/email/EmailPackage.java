5e9e204801850cf7c10411e47567034df9b336f2
Afraz Hussain <afrazhussain@live.com>
2019-10-09 20:01:48 +0100

    Remove override of createJSModules

--- android/src/main/java/com/facebook/react/modules/email/EmailPackage.java
+++ android/src/main/java/com/facebook/react/modules/email/EmailPackage.java
@@ -27,7 +27,6 @@ public class EmailPackage implements ReactPackage {
     return modules;
   }
 
-  @Override
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }

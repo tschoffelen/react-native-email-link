0c709f51b36bdcb074b51e5f9c984c9863d56a56
Thomas Schoffelen <thomas@schof.co>
2021-03-13 16:41:45 +0000

    Update Android package name. Fixes #35.

--- android/src/main/java/agency/flexible/react/modules/email/EmailModule.java
+++ android/src/main/java/agency/flexible/react/modules/email/EmailModule.java
@@ -1,4 +1,4 @@
-package com.facebook.react.modules.email;
+package agency.flexible.react.modules.email;
 
 import android.content.Intent;
 import android.content.pm.LabeledIntent;
--- android/src/main/java/agency/flexible/react/modules/email/EmailPackage.java
+++ android/src/main/java/agency/flexible/react/modules/email/EmailPackage.java
@@ -1,4 +1,4 @@
-package com.facebook.react.modules.email;
+package agency.flexible.react.modules.email;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptModule;
